561ed353119cb77fb21fbe1022969a555042c0aa
lim7635 <ju7635@naver.com>
2024-01-22 21:48:02 +0900

    2024.01.22 MatchCard 함수 추가

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,36 @@
 #define LEFT 75
 #define RIGHT 77
 #define DOWN 80
-#define SPACE 32
+#define SPACE 57
+
+#define BUFFER_SIZE 10000
+
+typedef struct SelectCard
+{
+	int x;
+	int y;
+	const char* shape;
+}SelectCard;
+
+// 색상을 결정하는 함수
+enum Color
+{
+	Black,  // 0 = 검정
+	Blue,   // 1 = 파랑
+	Green,  // 2 = 초록
+	Sky,    // 3 = 옥색
+	Red,    // 4 = 빨강
+	Purple, // 5 = 자주
+	Yellow, // 6 = 노랑
+	White,  // 7 = 흰색
+	Gray    // 8 = 회색
+};
+
+enum Button
+{
+	GameStart,
+	HowToPlay
+};
 
 // 버퍼의 크기
 int width = 100;
@@ -135,29 +164,70 @@ void PrintfScreen(int x, int y, const char* string)
 	);
 }
 
-// 색상을 결정하는 함수
-enum Color
+void LoadFile(const char* fileName)
 {
-	Black,  // 0 = 검정
-	Blue,   // 1 = 파랑
-	Green,  // 2 = 초록
-	Sky,    // 3 = 옥색
-	Red,    // 4 = 빨강
-	Purple, // 5 = 자주
-	Yellow, // 6 = 노랑
-	White,  // 7 = 흰색
-	Gray    // 8 = 회색
-};
+	// data.txt 파일을 열어주고 r(읽기) 모드로 설정합니다.
+	FILE* file = fopen(fileName, "r");
 
-// 카드 2차원 배열(40장)
-char Card[4][10];
+	char buffer[BUFFER_SIZE] = { 0, };
 
-typedef struct SelectCard
+	fread(buffer, 1, BUFFER_SIZE, file); // 전체 파일을 읽어주는 함수
+
+	printf("%s", buffer);
+
+	fclose(file);
+}
+
+void KeyboardMain(SelectCard* Select)
 {
-	int x;
-	int y;
-	const char* shape;
-}SelectCard;
+	char key = 0;
+	Button button;
+	if (_kbhit()) // 키보드 입력 확인 함수(true / false)
+	{
+		key = _getch(); // key 입력을 받아주는 함수
+		system("cls");
+
+		if (key == -32)
+		{
+			key = _getch();
+		}
+
+		switch (key)
+		{
+		case SPACE: switch (button)
+					{
+					case GameStart: SelectDiff();
+						break;
+					case HowToPlay: How();
+						break;
+					default:
+						break;
+					}
+			break;
+
+		case UP: Select->y--;
+			break;
+
+		case DOWN: Select->y++;
+			break;
+
+		default:
+			break;
+		}
+	}
+}
+
+void SelectDiff() // 난이도 선택
+{
+	printf("난이도 선택\n");
+	printf("Easy\tNormal\tHard");
+
+}
+
+void How() // 게임 방법 설명
+{
+	printf("1. 카드 한 장을 선택 후 다른 카드 한 장을 선택합니다.\n    * 처음 선택한 카드를 다시 선택한다면 처음 선택할 카드를 다시 선택할 수 있습니다.\n2. 두 장의 카드의 모양이 서로 같다면 카드는 사라지고 다르다면 사라지지 않습니다.")
+}
 
 // 카드 배치
 void CreateCard()
@@ -194,7 +264,14 @@ void Render()
 
 int main()
 {
-	SelectCard Select = { 0, 0, "□" };
+	SelectCard Select = { 10, 10, "□" };
 
+	LoadFile(".txt");
+	KeyboardMain(&Select);
+
+	while (1)
+	{
+
+	}
 	return 0;
 }
